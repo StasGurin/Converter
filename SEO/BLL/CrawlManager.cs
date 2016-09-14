a790dbd7fa7b13f098a4253207b8bffec37504b2
StasGurin <azgard0308@gmail.com>
2016-09-14 12:56:05 +0300

    Tine fix and rename

--- SEO/BLL/CrawlManager.cs
+++ SEO/BLL/CrawlManager.cs
@@ -3,10 +3,8 @@ using DataBase.Models.Crawls;
 using MongoDB.Bson;
 using MongoDB.Driver;
 using SEO.Models;
-using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 
 namespace SEO.BLL
 {
@@ -21,19 +19,19 @@ namespace SEO.BLL
             var filter = new BsonDocument();
             var result = dataBase.Crawls.Find(filter).ToListAsync().Result;
 
-            Crawls.AddRange(result.Select(crawls => new Crawl(crawls.Id, crawls.Name, crawls.IPListUrl, crawls.DomainMasks, crawls.IPs)));
+            Crawls.AddRange(result);
         }
 
         public static PageRenderingInfo RecognitionCrawl(VisitInfo visitInfo)
         {
 
-            foreach (var docCrawls in Crawls)
+            foreach (var crawl in Crawls)
             {
-                foreach (var docIPs in docCrawls.IPs)
+                foreach (var docIPs in crawl.IPs)
                 {
-                    if (docIPs.Address.StartsWith(visitInfo.IPAddress))
+                    if (visitInfo.IPAddress.StartsWith(docIPs.Address))
                     {
-                        return new PageRenderingInfo(docCrawls.Id, true);
+                        return new PageRenderingInfo(crawl.Id, true);
                     }
                 }
             }
